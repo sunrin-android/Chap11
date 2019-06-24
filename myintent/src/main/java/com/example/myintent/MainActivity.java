b35596b73f9212a7359f3baad18942b94cf217d6
Hyunwoo K <admin@hyunwoo.kim>
2019-06-24 15:07:00 +0900

    Add Camera, Gallery, Voice Recognition, Map, Browser, Phone

--- myintent/src/main/java/com/example/myintent/MainActivity.java
+++ myintent/src/main/java/com/example/myintent/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.myintent;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView text1;
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,82 @@ public class MainActivity extends AppCompatActivity {
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
         btn7 = findViewById(R.id.btn7);
-        
+        imageView = findViewById(R.id.image);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn1) {
+            // 주소록 앱 연동
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setData(ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, 100);
+        } else if (v == btn2) {
+            // 카메라 앱 연동
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // 암시적 인텐트
+            startActivityForResult(intent, 200);
+        } else if (v == btn3) {
+            // 갤러리 앱 연동
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 300);
+        } else if (v == btn4) {
+            // 음성 인식
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "음성인식 테스트");
+        } else if (v == btn5) {
+            // 지도 연동
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:37.5662952, 126.9779451?q=37.5662952, 126.9779451"));
+            startActivity(intent);
+        } else if (v == btn6) {
+            // 브라우저
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://sunrint.hs.kr"));
+            startActivity(intent);
+        } else if (v == btn7) {
+            // 전화걸기
+            // 권한체크
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:02-713-6213"));
+                startActivity(intent);
+            } else {
+                // 대화창으로 사용자에게 요청
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 10);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100 && requestCode == RESULT_OK) {
+            String result = data.getDataString();
+            text1.setText(result);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(result));
+            startActivity(intent);
+        } else if (requestCode == 200 && requestCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        } else if (requestCode == 300 && requestCode == RESULT_OK) {
+            String result = data.getDataString();
+            text1.setText(result);
+        } else if (requestCode == 400 && requestCode == RESULT_OK) {
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String result = results.get(0);
+            text1.setText(result);
+        }
     }
 }
